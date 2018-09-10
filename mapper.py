1d8e52899b190b45cdf82559edb09a240c307d98
romybeugeling <romybeugeling@live.nl>
2018-09-10 10:26:20 +0200

    mapper

--- mapper.py
+++ mapper.py
@@ -5,31 +5,18 @@ import sys
 
 def mapper():
 
+    word = "fantastic"
+
     # Input comes from STDIN (standard input)
     for line in sys.stdin:
 
-        row = line.strip().split(';')
-        # Check for the amount of columns we need
-        if len(row) < 26:
-            continue
+        # remove characters 
+        row = line.strip().split('.', ',', '!', '?', ':', ';', '"', 
+        '(', ')', '<', '>', '[', ']', '#', '$', '=', '-','/' )
 
-        # Identify variables
-        gender = row[0]
-        ageGroup = row[1]
-        municipality = row[2]
-        amountBSN = row[3]
-        costCare = row[25]
+        row.lower()
 
-        if gender != 'M' and gender != 'V':
-            continue
-
-        # Check if age group is elderly
-        minAge = ageGroup[:2]
-        if minAge.isdigit() and int(minAge) >= 65:
-
-            amountMale = amountBSN if gender == 'M' else 0
-            amountFemale = amountBSN if gender == 'V' else 0
-
-            print("{0};{1};{2};{3}".format(municipality, amountMale, amountFemale, costCare))
+        if word in row:
+            print("{0}".format(word))
 
 mapper()

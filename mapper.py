d7ac77dfb7b0ad713fcd1a04d99bc460683c7b11
romybeugeling <romybeugeling@live.nl>
2018-09-10 12:46:43 +0200

    mapper

--- mapper.py
+++ mapper.py
@@ -9,13 +9,20 @@ def mapper():
     for line in sys.stdin:
 
         # remove characters 
-        row = line.split()
+        row = line.split('\t')
 
         for word in row:
             # if not a numeric character remove
-            word = ''.join(e for e in word if e.isalnum())
-            # if word not empty print
-            if word:
-                print('{0};{1}'.format(word.lower(), 1))
+            # word = ''.join(e for e in word if e.isalnum())
+
+            for ch in "['#','`','\','-','=','!']":
+                if ch in word:  
+                    word = word.replace(ch," ")
+
+        word = word.split(" ")
+
+        # if word not empty print
+        if word:
+            print('{0};{1}'.format(word.lower(), 1))
 
 mapper()

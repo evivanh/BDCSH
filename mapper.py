cb13e8b1803255e005eb424599bc6b30609b5721
romybeugeling <romybeugeling@live.nl>
2018-09-10 11:19:45 +0200

    split

--- mapper.py
+++ mapper.py
@@ -12,7 +12,7 @@ def mapper():
         # remove characters 
         row = line.split()
         print (row)
-        row = re.split(r'[`\-=!#$()+\[\];\'\\:"<,./<>?\t\r\n\N]', row)
+        row = re.split(r'[`\-=!#$()+\[\];\'\\:"<,./<>?]', row)
     
         # print(line)
         # for word in row: 

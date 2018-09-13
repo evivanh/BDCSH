a23cf40c040c4f8b66ac51100fa5107576119ee0
romybeugeling <romybeugeling@live.nl>
2018-09-13 14:14:27 +0200

    print

--- assignment1/1.2/reducer.py
+++ assignment1/1.2/reducer.py
@@ -32,7 +32,9 @@ def reducer():
         currentHourOfDay = data[3] if data[3] != None else None
         currentAmountPerHour = 1
 
+        print("a")
         print(printTemplate.format(currentUserId, currentFirstName, currentLastName, currentHourOfDay))
+        print("b")
         print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay))
 
         if previousUserId and previousUserId != currentUserId:

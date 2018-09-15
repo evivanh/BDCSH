dd1de15d2ccb79eb528381144500bc587a92b59c
romybeugeling <romybeugeling@live.nl>
2018-09-15 21:48:45 +0200

    got code from two days ago

--- assignment1/1.2/reducer.py
+++ assignment1/1.2/reducer.py
@@ -48,31 +48,31 @@ def reducer():
 
         # if previous id exists and is not the same as current id print it and reset variables
         # firstname, lastname, none, 0 fails here
-        if previousUserId and previousUserId != currentUserId:
-            print("first")
-            print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
-            previousAmountPerHour = 0
-            previousUserId = None
-            previousFirstName = None
-            previousLastName = None
-            previousHourOfDay = None
-        # if previous id is same as current id and currenhour and previoushour exist and are not the same print and set previous hour of day and reset counter
-        # none, none, 01, 939 fails here
-        if previousUserId == currentUserId and currentHourOfDay and previousHourOfDay == currentHourOfDay:
-            previousHourOfDay = currentHourOfDay
-            previousAmountPerHour += currentAmountPerHour
-        elif previousUserId == currentUserId and currentHourOfDay and previousHourOfDay and previousHourOfDay != currentHourOfDay:
-            print("second")
-            print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
-            previousHourOfDay = currentHourOfDay
-            previousAmountPerHour = currentAmountPerHour
-        # elif previousUserId == currentUserId and currentHourOfDay and previousHourOfDay == currentHourOfDay:
+        # if previousUserId and previousUserId != currentUserId:
+        #     print("first")
+        #     print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+        #     previousAmountPerHour = 0
+        #     previousUserId = None
+        #     previousFirstName = None
+        #     previousLastName = None
+        #     previousHourOfDay = None
+        # # if previous id is same as current id and currenhour and previoushour exist and are not the same print and set previous hour of day and reset counter
+        # # none, none, 01, 939 fails here
+        # if previousUserId == currentUserId and currentHourOfDay and previousHourOfDay == currentHourOfDay:
         #     previousHourOfDay = currentHourOfDay
         #     previousAmountPerHour += currentAmountPerHour
-        # previous id is not same as curren id and current firstname and lastname exist set the names
-        if previousUserId != currentUserId and currentFirstName and currentLastName:
-            previousFirstName = currentFirstName
-            previousLastName = currentLastName
+        # elif previousUserId == currentUserId and currentHourOfDay and previousHourOfDay and previousHourOfDay != currentHourOfDay:
+        #     print("second")
+        #     print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+        #     previousHourOfDay = currentHourOfDay
+        #     previousAmountPerHour = currentAmountPerHour
+        # # elif previousUserId == currentUserId and currentHourOfDay and previousHourOfDay == currentHourOfDay:
+        # #     previousHourOfDay = currentHourOfDay
+        # #     previousAmountPerHour += currentAmountPerHour
+        # # previous id is not same as curren id and current firstname and lastname exist set the names
+        # if previousUserId != currentUserId and currentFirstName and currentLastName:
+        #     previousFirstName = currentFirstName
+        #     previousLastName = currentLastName
         # if previousUserId == currentUserId and currentFirstName and currentLastName:
         #     previousFirstName = currentFirstName
         #     previousLastName = currentLastName
@@ -82,7 +82,25 @@ def reducer():
         #     previousAmountPerHour += currentAmountPerHour
 
 
-        
+        if previousUserId and previousUserId != currentUserId:
+            print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+            previousAmountPerHour = 0
+            previousUserId = None
+            previousFirstName = None
+            previousLastName = None
+            previousHourOfDay = None
+        # maybe good
+        if previousUserId == currentUserId and currentHourOfDay and previousHourOfDay and previousHourOfDay != currentHourOfDay:
+            print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+            previousHourOfDay = currentHourOfDay
+            previousAmountPerHour = 0
+        # good
+        if previousUserId != currentUserId and currentFirstName and currentLastName:
+            previousFirstName = currentFirstName
+            previousLastName = currentLastName
+        if previousUserId == currentUserId and currentHourOfDay:
+            previousHourOfDay = currentHourOfDay
+            previousAmountPerHour += currentAmountPerHour
 
         previousUserId = currentUserId
         # previousFirstName = currentFirstName

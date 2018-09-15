acdfd735954e074264f7901b31885e31cf032dc5
romybeugeling <romybeugeling@live.nl>
2018-09-15 23:58:19 +0200

    Test print.

--- assignment1/1.2/reducer.py
+++ assignment1/1.2/reducer.py
@@ -20,17 +20,19 @@ def reducer():
     # Input comes from STDIN
     for line in sys.stdin:
 
+        print(line)
+
         # Check argument count
-        data = line.strip().split(';')
+        # data = line.strip().split(';')
 
-        if len(data) < 4:
-            continue
+        # if len(data) < 4:
+        #     continue
 
-        currentUserId = data[0]
-        currentFirstName = data[1] if data[1] != None else None
-        currentLastName = data[2] if data[2] != None else None
-        currentHourOfDay = data[3] if data[3] != None else None
-        currentAmountPerHour = 1
+        # currentUserId = data[0]
+        # currentFirstName = data[1] if data[1] != None else None
+        # currentLastName = data[2] if data[2] != None else None
+        # currentHourOfDay = data[3] if data[3] != None else None
+        # currentAmountPerHour = 1
 
         # if currentFirstName and previousFirstName == None:
         #     previousFirstName = currentFirstName
@@ -61,36 +63,36 @@ def reducer():
 
 
         # last valid stuff
-        if previousUserId and previousUserId != currentUserId:
-            print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
-            previousAmountPerHour = 0
-            previousUserId = None
-            previousFirstName = None
-            previousLastName = None
-            previousHourOfDay = None
-        # maybe good
-        if previousUserId == currentUserId and currentHourOfDay and previousHourOfDay and previousHourOfDay != currentHourOfDay:
-            print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
-            previousHourOfDay = currentHourOfDay
-            previousAmountPerHour = 0
-        # good
-        if previousUserId != currentUserId and currentFirstName and currentLastName:
-            previousFirstName = currentFirstName
-            previousLastName = currentLastName
-        if previousUserId == currentUserId and currentHourOfDay:
-            previousHourOfDay = currentHourOfDay
-            previousAmountPerHour += currentAmountPerHour
-        # if previousUserId == currentUserId and currentFirstName and currentLastName:
-        #     previousFirstName = currentFirstName
-        #     previousLastName = currentLastName
-        previousUserId = currentUserId
-        # previousFirstName = currentFirstName
-        # previousLastName = currentLastName
-        # previousHourOfDay = currentHourOfDay
+    #     if previousUserId and previousUserId != currentUserId:
+    #         print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+    #         previousAmountPerHour = 0
+    #         previousUserId = None
+    #         previousFirstName = None
+    #         previousLastName = None
+    #         previousHourOfDay = None
+    #     # maybe good
+    #     if previousUserId == currentUserId and currentHourOfDay and previousHourOfDay and previousHourOfDay != currentHourOfDay:
+    #         print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+    #         previousHourOfDay = currentHourOfDay
+    #         previousAmountPerHour = 0
+    #     # good
+    #     if previousUserId != currentUserId and currentFirstName and currentLastName:
+    #         previousFirstName = currentFirstName
+    #         previousLastName = currentLastName
+    #     if previousUserId == currentUserId and currentHourOfDay:
+    #         previousHourOfDay = currentHourOfDay
+    #         previousAmountPerHour += currentAmountPerHour
+    #     # if previousUserId == currentUserId and currentFirstName and currentLastName:
+    #     #     previousFirstName = currentFirstName
+    #     #     previousLastName = currentLastName
+    #     previousUserId = currentUserId
+    #     # previousFirstName = currentFirstName
+    #     # previousLastName = currentLastName
+    #     # previousHourOfDay = currentHourOfDay
    
-    # Print the current word and its count
-    if previousFirstName and previousLastName and previousHourOfDay and previousAmountPerHour:
-        print("third")
-        print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+    # # Print the current word and its count
+    # if previousFirstName and previousLastName and previousHourOfDay and previousAmountPerHour:
+    #     print("third")
+    #     print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
 
 reducer()

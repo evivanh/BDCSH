14fb948c43c99594abc10f48f997698f81bed7ca
romybeugeling <romybeugeling@live.nl>
2018-09-16 12:19:27 +0200

    p

--- assignment1/1.2/reducer.py
+++ assignment1/1.2/reducer.py
@@ -31,71 +31,71 @@ def reducer():
         currentHourOfDay = data[3] if data[3] != None else None
         listenCount = 1
 
-        # if currentFirstName and previousFirstName == None:
-        #     previousFirstName = currentFirstName
+        if currentFirstName and previousFirstName == None:
+            previousFirstName = currentFirstName
 
-        # if currentLastName and previousLastName == None:
-        #     previousLastName = currentLastName
+        if currentLastName and previousLastName == None:
+            previousLastName = currentLastName
 
-        # if previousUserId and previousUserId != currentUserId:
-        #     print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
-        #     previousAmountPerHour = 0
-        #     previousUserId = None
-        #     previousFirstName = None
-        #     previousLastName = None
-        #     previousHourOfDay = None
-        # if currentHourOfDay and currentHourOfDay == previousHourOfDay:
-        #     previousAmountPerHour += currentAmountPerHour
-        # if currentHourOfDay and previousHourOfDay and previousHourOfDay != currentHourOfDay:
-        #     print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
-        #     previousHourOfDay = currentHourOfDay
-        #     previousAmountPerHour = 0
-
-        # if previousUserId != currentUserId and currentFirstName and currentLastName:
-        #     previousFirstName = currentFirstName
-        #     previousLastName = currentLastName
-
-
-        # previousUserId = currentUserId
-
-
-        # Changed user?
         if previousUserId and previousUserId != currentUserId:
-            # Print previous last line
             print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
-            # Reset vars
             previousAmountPerHour = 0
             previousUserId = None
             previousFirstName = None
             previousLastName = None
             previousHourOfDay = None
+        if currentHourOfDay and currentHourOfDay == previousHourOfDay:
+            previousAmountPerHour += listenCount
+        if currentHourOfDay and previousHourOfDay and previousHourOfDay != currentHourOfDay:
+            print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+            previousHourOfDay = currentHourOfDay
+            previousAmountPerHour = 0
 
-        # Same user but hour of day changed?
-        # if previousUserId == currentUserId and previousHourOfDay and previousHourOfDay != currentHourOfDay:
-        #     # Print current user and previous hour of day
-        #     print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
-        #     # Reset var
-        #     previousAmountPerHour = 0
-        
-        # Set name of current user
-        if currentFirstName and currentLastName:
+        if previousUserId != currentUserId and currentFirstName and currentLastName:
             previousFirstName = currentFirstName
             previousLastName = currentLastName
-        
-        # If line has an hour of day
-        if currentHourOfDay:
-            # Same user and hour of day? Increase count
-            if previousUserId == currentUserId and previousHourOfDay == currentHourOfDay:
-                previousAmountPerHour += listenCount
-            elif previousHourOfDay != currentHourOfDay:
-                # Print current user and previous hour of day
-                print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
-                # Reset var
-                previousAmountPerHour = 0
 
-            previousHourOfDay = currentHourOfDay
-            
+
         previousUserId = currentUserId
+
+
+        # # Changed user?
+        # if previousUserId and previousUserId != currentUserId:
+        #     # Print previous last line
+        #     print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+        #     # Reset vars
+        #     previousAmountPerHour = 0
+        #     previousUserId = None
+        #     previousFirstName = None
+        #     previousLastName = None
+        #     previousHourOfDay = None
+
+        # # Same user but hour of day changed?
+        # # if previousUserId == currentUserId and previousHourOfDay and previousHourOfDay != currentHourOfDay:
+        # #     # Print current user and previous hour of day
+        # #     print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+        # #     # Reset var
+        # #     previousAmountPerHour = 0
+        
+        # # Set name of current user
+        # if currentFirstName and currentLastName:
+        #     previousFirstName = currentFirstName
+        #     previousLastName = currentLastName
+        
+        # # If line has an hour of day
+        # if currentHourOfDay:
+        #     # Same user and hour of day? Increase count
+        #     if previousUserId == currentUserId and previousHourOfDay == currentHourOfDay:
+        #         previousAmountPerHour += listenCount
+        #     elif previousHourOfDay != currentHourOfDay:
+        #         # Print current user and previous hour of day
+        #         print(printTemplate.format(previousUserId, previousFirstName, previousLastName, previousHourOfDay, previousAmountPerHour))
+        #         # Reset var
+        #         previousAmountPerHour = 0
+
+        #     previousHourOfDay = currentHourOfDay
+
+        # previousUserId = currentUserId
    
     # Print the current word and its count
     if previousFirstName and previousLastName and previousHourOfDay and previousAmountPerHour:

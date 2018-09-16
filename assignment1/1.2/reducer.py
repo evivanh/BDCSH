f9bfff1a3fd4656c79d46a72eb96a256c8006172
romybeugeling <romybeugeling@live.nl>
2018-09-16 13:09:37 +0200

    check if 'none' as string not None

--- assignment1/1.2/reducer.py
+++ assignment1/1.2/reducer.py
@@ -26,9 +26,9 @@ def reducer():
             continue
 
         currentUserId = data[0]
-        currentFirstName = data[1]
-        currentLastName = data[2]
-        currentHourOfDay = data[3]
+        currentFirstName = data[1] if data[1] != 'None' else None
+        currentLastName = data[2] if data[1] != 'None' else None
+        currentHourOfDay = data[3] if data[1] != 'None' else None
         listenCount = 1
 
         # if currentFirstName and previousFirstName == None:

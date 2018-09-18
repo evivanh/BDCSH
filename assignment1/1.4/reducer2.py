33d849fd2324ea38daea5ffc8a3fba79054c652a
romybeugeling <romybeugeling@live.nl>
2018-09-18 18:49:05 +0200

    prints

--- assignment1/1.4/reducer2.py
+++ assignment1/1.4/reducer2.py
@@ -31,8 +31,8 @@ def reducer():
         currentArtist = data[5] if data[5] != 'None' else None
 
         if userId and userId != currentUserId:
-            for track in userTracks.items():
-                print(printTemplate.format(track[0], userId, firstName, lastName, track[1][INDEX_TRACK_COUNT], track[1][INDEX_TRACK_ARTIST]))
+            # for track in userTracks.items():
+            #     print(printTemplate.format(track[0], userId, firstName, lastName, track[1][INDEX_TRACK_COUNT], track[1][INDEX_TRACK_ARTIST]))
 
             firstName = None
             lastName = None
@@ -44,11 +44,14 @@ def reducer():
                 
                 if currentTrackCount != 0:
                     userTracks[currentTrackId][INDEX_TRACK_COUNT] = currentTrackCount
+                    print("1 " , userTracks[currentTrackId])
                 if currentArtist:
                     userTracks[currentTrackId][INDEX_TRACK_ARTIST] = currentArtist
+                    print("2 " , userTracks[currentTrackId])
+
             else:
                 userTracks[currentTrackId] = [0, None]
-                print (userTracks[currentTrackId])
+                print ("3 " , userTracks[currentTrackId])
 
         if currentFirstName and currentLastName:
             firstName = currentFirstName
@@ -57,8 +60,8 @@ def reducer():
         userId = currentUserId
    
     # Print track id, user id, first name, last name, count and artist combined per track
-    for track in userTracks.items():
-        print(printTemplate.format(track[0], userId, firstName, lastName, track[1][INDEX_TRACK_COUNT], track[1][INDEX_TRACK_ARTIST]))
+    # for track in userTracks.items():
+        # print(printTemplate.format(track[0], userId, firstName, lastName, track[1][INDEX_TRACK_COUNT], track[1][INDEX_TRACK_ARTIST]))
 
 reducer()
 

da44f531b321e76782074f0d4b763852a1e2891b
Evi van Heijningen <evivanheijningen@live.nl>
2018-09-20 11:11:32 +0200

    reducer/mapper added

--- assignment1.2/MapperShakespeare.java
+++ assignment1.2/MapperShakespeare.java
@@ -1,5 +1,26 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 /**
  * Created by Evi on 20-9-2018.
  */
-public class MapperShakespeare {
+public class MapperShakespeare extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Mapper.Context context
+    ) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+        }
+    }
 }
+
+

66a7097bc0e7cf504d4729a87b815a6d8f2e0293
Evi van Heijningen <evivanheijningen@live.nl>
2018-09-20 15:20:14 +0200

    filesplit different import

--- assignment1.2/MapperShakespeare.java
+++ assignment1.2/MapperShakespeare.java
@@ -1,7 +1,7 @@
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 import java.io.IOException;
 

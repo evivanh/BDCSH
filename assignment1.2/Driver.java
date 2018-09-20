2277d9ff95c9f059d73a58ebff902b3df9784777
Evi van Heijningen <evivanheijningen@live.nl>
2018-09-20 15:37:45 +0200

    driver edit

--- assignment1.2/Driver.java
+++ assignment1.2/Driver.java
@@ -27,7 +27,7 @@ public class Driver {
         //job.setCombinerClass(ReducerShakespeare.class);
         job.setReducerClass(ReducerShakespeare.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);

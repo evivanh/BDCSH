08a1a9c911744732fd47b867e6a889a38970dd7c
romybeugeling <romybeugeling@live.nl>
2018-09-20 11:04:42 +0200

    driver

--- assignment1.2/Driver.java
+++ assignment1.2/Driver.java
@@ -8,26 +8,24 @@ import org.apache.hadoop.mapreduce.Job;
 public class Driver {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
-            System.out.printf("Usage: WordCount <input dir> <output dir>\n");
-            System.exit(-1)
+            System.out.println("Usege: WordCount <input dir> <output dir>\n");
+            System.exit(-1);
         }
 
-        Word Count
-        Driver (2)
-        Job job = new Job();
-        job.setJarByClass(WordCount.class);
-        job.setJobName("Word Count");
-        FileInputFormat.setInputPaths(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setMapperClass(WordMapper.class); /* Mapper */
-        job.setReducerClass(SumReducer.class); /* Reducer */
-/*Intermediate key/value types: */
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
-/* Output value types of Reducer: */
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        boolean success = job.waitForCompletion(true);
-        System.exit(success ? 0 : 1);
+//        Job job = new Job();
+//        job.setJarByClass(WordCount.class);
+//        job.setJobName("Word Count");
+//        FileInputFormat.setInputPaths(job, new Path(args[0]));
+//        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+//        job.setMapperClass(WordMapper.class); /* Mapper */
+//        job.setReducerClass(SumReducer.class); /* Reducer */
+///*Intermediate key/value types: */
+//        job.setMapOutputKeyClass(Text.class);
+//        job.setMapOutputValueClass(IntWritable.class);
+///* Output value types of Reducer: */
+//        job.setOutputKeyClass(Text.class);
+//        job.setOutputValueClass(IntWritable.class);
+//        boolean success = job.waitForCompletion(true);
+//        System.exit(success ? 0 : 1);
     }
 }

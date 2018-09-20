7fb397052fcd312194e56b001966f3123d31605a
Evi van Heijningen <evivanheijningen@live.nl>
2018-09-20 15:31:42 +0200

    type mismatch fix

--- assignment1.2/MapperShakespeare.java
+++ assignment1.2/MapperShakespeare.java
@@ -8,12 +8,12 @@ import java.io.IOException;
 /**
  * Created by Evi on 20-9-2018.
  */
-public class MapperShakespeare extends Mapper<LongWritable, Text, Text, Text> {
+public class MapperShakespeare extends Mapper<Text, Text, Text, Text> {
 
 //    private final static IntWritable one = new IntWritable(1);
 //    private Text line = new Text();
 
-    public void map(LongWritable key, Text value, Context context
+    public void map(Text key, Text value, Context context
     ) throws IOException, InterruptedException {
         String line = value.toString();
         String fileName = ((FileSplit)context.getInputSplit()).getPath().getName();
